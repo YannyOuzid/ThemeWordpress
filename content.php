97066d144765c382b1e596d305592fceb2b55b4e
yannyiim <55101971+yannyiim@users.noreply.github.com>
2020-04-22 00:28:56 +0200

    theme

--- content.php
+++ content.php
@@ -1,6 +1,8 @@
 <h1><?php the_title(); ?></h1>
 
-    <?php the_time('d/m/Y'); ?>
+    <p>Date : <?php the_time('d/m/Y'); ?></p>
+
+    <p>Auteur : <?php the_author(); ?></p>
 
     <?php the_content(  ); ?> 
     
--- home.php
+++ home.php
@@ -17,7 +17,9 @@
     <?php the_post_thumbnail(); ?>
     <h2><?php the_title(); ?></h2>
     </a>
-    <?php the_time('d/m/Y'); ?>
+    <p>Date : <?php the_time('d/m/Y'); ?></p>
+
+    <p>Auteur : <?php the_author(); ?></p>
     <?php the_excerpt(  ); ?> 
     <a href="<?php the_permalink( ); ?>">
     Lire
