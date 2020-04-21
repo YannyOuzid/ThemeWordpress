05998f46a9a19406081fdf10369cbefc0f5ffd07
yannyiim <55101971+yannyiim@users.noreply.github.com>
2020-04-21 18:57:44 +0200

    theme

---
+++ category.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+<?php get_sidebar(); ?>
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+    <article>
+
+    <a href="<?php the_permalink( ); ?>">
+    <?php the_post_thumbnail(); ?>
+    <h2><?php the_title(); ?></h2>
+    </a>
+    <?php the_excerpt(  ); ?> 
+    <a href="<?php the_permalink( ); ?>">
+    Lire
+    </a>
+
+    </article>
+ 
+    <?php endwhile; ?>             
+  
+
+        <div class="archive-search-form"><?php get_search_form(); ?></div>
+
+        <h2>Archives par ans:</h2>
+        <ul><?php wp_get_archives('type=yearly'); ?></ul>
+
+        <h2>Archives par mois:</h2>
+        <ul><?php wp_get_archives('type=monthly'); ?></ul>
+
+        <h2>Archives par sujet:</h2>
+        <ul><?php wp_list_categories('title_li='); ?></ul>
+      </div><!-- #entry-content -->
+
+
+<?php get_footer(); ?>
