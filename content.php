dd147672b0eafc9ab1ba43bf7e11a9034022f7d5
yannyiim <55101971+yannyiim@users.noreply.github.com>
2020-05-18 16:37:50 +0200

    content 16H37

--- content.php
+++ content.php
@@ -1,13 +1,24 @@
 <div class="text-center"><h1><?php the_title(); ?></h1></div>
 
-<div class="card" style="width: 18rem; background-color: transparent; border-color: #F06449">
-  <div class="card-body">
+</br>
+
+<div class="container">
+
+    <div style="display:flex; justify-content:center">
+
+        <div class="card" style="width: 18rem; background-color: transparent; border-color: #F06449">
+            <div class="card-body">
+                <p class="card-text">Date : <?php the_time('d/m/Y'); ?></p>
+                <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">Auteur : <?php the_author(); ?></a>
+                <p class="card-text"><?php the_tags(); ?></p>
+            </div>
+        </div>
     
-    <p class="card-text">Date : <?php the_time('d/m/Y'); ?></p>
-    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">Auteur : <?php the_author(); ?></a>
-    <p class="card-text"><?php the_tags(); ?></p>
-  </div>
+    </div>
+
 </div>
+
+</br>
 
 <div class="container">
     <?php the_content(  ); ?>
