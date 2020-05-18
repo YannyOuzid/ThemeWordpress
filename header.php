0743ceadf00c1c514680a116587cddb543f2e09d
yannyiim <55101971+yannyiim@users.noreply.github.com>
2020-05-18 10:42:58 +0200

    mise a jour

--- header.php
+++ header.php
@@ -16,11 +16,10 @@
 
         <nav id="navigation-principale" role="navigation">
 
-
+            <div class="justify-content-center">
                 <?php wp_nav_menu( array( 'theme_location' => 'menu-principal' ) ); ?>
+            </div>
 
-
-       </nav>
-       
+	   </nav>
 
 	</header>
