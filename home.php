6bf951c0546a3f06b5d750e190d18d43253759ca
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-05-18 11:42:54 +0200

    Push 11h42

--- home.php
+++ home.php
@@ -6,10 +6,40 @@
 
     <div class="container">
 
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                
+                <div class="justify-content-center">
+
+                    <img src="https://wpengine.com/wp-content/uploads/2018/09/change-theme-resource1.png" alt="Image d'exemple" class="img-example">
+
+                </div>
+
+            </div>
+
+            <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                <div class="text-center">
+                    <h2>Lorem ipsum dolor...</h2>
+                </div>
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>            
+        </div>
+
         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-
             <h2>Articles du blog</h2>
-
         </div>
 
         <hr>
