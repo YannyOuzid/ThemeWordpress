41f2caaf39290bb99d952b5e3245ec4d43586c88
yannyiim <55101971+yannyiim@users.noreply.github.com>
2020-05-18 10:07:20 +0200

    ajout card article

--- author.php
+++ author.php
@@ -8,14 +8,14 @@
 
     <article>
 
-    <a href="<?php the_permalink( ); ?>">
-    <?php the_post_thumbnail(); ?>
-    <h2><?php the_title(); ?></h2>
-    </a>
-    <?php the_excerpt(  ); ?> 
-    <a href="<?php the_permalink( ); ?>">
-    Lire
-    </a>
+        <a href="<?php the_permalink( ); ?>">
+        <?php the_post_thumbnail(); ?>
+        <h2><?php the_title(); ?></h2>
+        </a>
+        <?php the_excerpt(  ); ?> 
+        <a href="<?php the_permalink( ); ?>">
+        Lire
+        </a>
 
     </article>
  
