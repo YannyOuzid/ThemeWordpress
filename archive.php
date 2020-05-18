7f13222335617a5476b098774ca22eaa60b6bbec
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-05-18 12:08:38 +0200

    Push 12h08

--- archive.php
+++ archive.php
@@ -2,34 +2,49 @@
 
 <?php get_sidebar(); ?>
 
-    <h1>Archives</h1>
+    <div class="container">
 
-    <?php while ( have_posts() ) : the_post(); ?>
+        <div class="text-center">
 
-    <div class="row">
+            <h2>Archives</h2>
 
-    <article style="padding-right:20px">
-
-    <div class="card" style="width: 18rem; background-color: black; border-color: #F06449" >
-        <img class="card-img-top" src="..." alt="Card image cap">
-  
-        <a href="<?php the_permalink( ); ?>">
-        <?php the_post_thumbnail(); ?>
-        </a>
-
-        <div class="card-body">
-            <h5 class="card-title"><?php the_title(); ?></h5>
-            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">Auteur : <?php the_author(); ?></a>
-            <p class="card-text"><?php the_tags(); ?></p>
-            <p class="card-text">Date : <?php the_time('d/m/Y'); ?></p>
-            <p class="card-text"><?php the_excerpt(  ); ?> </p>
-            <a href="<?php the_permalink( ); ?>" class="btn btn-primary">Lire</a>
         </div>
-    </div>
 
-    </article>
+        <hr>
+
+        <div class="row-extended">
+
+            <?php while ( have_posts() ) : the_post(); ?>
+
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">        
+
+                <article style="padding-right:20px">
+
+                    <div class="card" style="width: 100%; background-color: black; border-color: #F06449" >
+                        <img class="card-img-top" src="..." alt="Card image cap">
+      
+                        <a href="<?php the_permalink( ); ?>">
+                            <?php the_post_thumbnail(); ?>
+                        </a>
+
+                        <div class="card-body">
+                            <h5 class="card-title"><?php the_title(); ?></h5>
+                            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">Auteur : <?php the_author(); ?></a>
+                            <p class="card-text"><?php the_tags(); ?></p>
+                            <p class="card-text">Date : <?php the_time('d/m/Y'); ?></p>
+                            <p class="card-text"><?php the_excerpt(  ); ?> </p>
+                            <a href="<?php the_permalink( ); ?>" class="btn btn-primary">Lire</a>
+                        </div>
+
+                    </div>
+
+                </article>
+
+            </div>
  
-    <?php endwhile; ?>             
+            <?php endwhile; ?>  
+
+        </div>           
 
     </div>
 
--- date.php
+++ date.php
@@ -2,34 +2,46 @@
 
 <?php get_sidebar(); ?>
 
-    <h1>Archive par date</h1>
+    <div class="container">
 
-    <div class="row-extended">
+        <div class="text-center">
 
-        <?php while ( have_posts() ) : the_post(); ?>
+            <h2>Archive par date</h2>
 
-        <article class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
-            
-            <div class="card" style="width:100%; background-color: black; border-color: #F06449" >
+        </div>
 
-                <img class="card-img-top" src="..." alt="Card image cap">
+        <hr>
+
+        <div class="row-extended">
+
+            <?php while ( have_posts() ) : the_post(); ?>
+
+            <article class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+
+                <div class="card" style="width:100%; background-color: black; border-color: #F06449" >
+
+                    <img class="card-img-top" src="..." alt="Card image cap">
   
-                <a href="<?php the_permalink( ); ?>">
-                    <?php the_post_thumbnail(); ?>
-                </a>
+                    <a href="<?php the_permalink( ); ?>">
+                        <?php the_post_thumbnail(); ?>
+                    </a>
 
-                <div class="card-body">
-                    <h5 class="card-title"><?php the_title(); ?></h5>
-                    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">Auteur : <?php the_author(); ?></a>
-                    <p class="card-text"><?php the_tags(); ?></p>
-                    <p class="card-text">Date : <?php the_time('d/m/Y'); ?></p>
-                    <p class="card-text"><?php the_excerpt(  ); ?> </p>
-                    <a href="<?php the_permalink( ); ?>" class="btn btn-primary">Lire</a>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title(); ?></h5>
+                        <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">Auteur : <?php the_author(); ?></a>
+                        <p class="card-text"><?php the_tags(); ?></p>
+                        <p class="card-text">Date : <?php the_time('d/m/Y'); ?></p>
+                        <p class="card-text"><?php the_excerpt(  ); ?> </p>
+                        <a href="<?php the_permalink( ); ?>" class="btn btn-primary">Lire</a>
+                    </div>
+
                 </div>
-            </div>
-        </article>
+
+            </article>
  
-    <?php endwhile; ?> 
+        <?php endwhile; ?> 
+
+        </div>
     
     </div>
 
